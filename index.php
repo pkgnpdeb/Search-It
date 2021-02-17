6e1d748ab438fa84037c81d0b174b43c27b81493
id10t.1rL <48232101+pkgnpdeb@users.noreply.github.com>
2021-02-17 12:21:44 +0545

    Index Page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to Search-it</title>
+</head>
+<body>
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+        </div>
+        <div class="searchContainer">
+            <form action="search.php" method="GET">
+                <input type="text" class="searchBox" name="term" placeholder="Enter a Search term ...">
+                <input type="submit" class="searchButton" value="Search">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
